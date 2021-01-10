613fab3d72d1cdd2947f2efefb549516d3d7a86d
Changbeam Tak <tak.changbeom@gmail.com>
2021-01-10 16:14:25 +0900

    add sample json structure

--- src/api/github.ts
+++ src/api/github.ts
@@ -42,3 +42,45 @@ export interface GithubProfile {
   created_at:          Date;
   updated_at:          Date;
 }
+
+/*
+
+{
+  "login": "velopert",
+  "id": 17202261,
+  "node_id": "MDQ6VXNlcjE3MjAyMjYx",
+  "avatar_url": "https://avatars0.githubusercontent.com/u/17202261?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/velopert",
+  "html_url": "https://github.com/velopert",
+  "followers_url": "https://api.github.com/users/velopert/followers",
+  "following_url": "https://api.github.com/users/velopert/following{/other_user}",
+  "gists_url": "https://api.github.com/users/velopert/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/velopert/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/velopert/subscriptions",
+  "organizations_url": "https://api.github.com/users/velopert/orgs",
+  "repos_url": "https://api.github.com/users/velopert/repos",
+  "events_url": "https://api.github.com/users/velopert/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/velopert/received_events",
+  "type": "User",
+  "site_admin": false,
+  "name": "Minjun Kim",
+  "company": "@laftel-team ",
+  "blog": "https://velopert.com/",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": "개발은 언제나 즐겁고 재밌어야 한다는 생각을 갖고 있는 개발자이며, 가르치는것을 굉장히 좋아하는 교육자이기도 합니다.",
+  "twitter_username": null,
+  "public_repos": 71,
+  "public_gists": 31,
+  "followers": 1792,
+  "following": 17,
+  "created_at": "2016-02-12T16:43:22Z",
+  "updated_at": "2021-01-06T22:19:46Z"
+}
+
+https://api.github.com/users/velopert
+
+bio: short biography
+*/
